b52017da5dcee3b6e24e42d0749796ebf4c6488e
S0F1AX <107075867+S0F1AX@users.noreply.github.com>
2023-11-27 17:58:09 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,93 @@
+adesivo ="";
+caneca ="";
+escultura ="";
+lego ="";
+popit ="";
+modelstatus ="";
+
+function preload()
+{
+    adesivo = loadImage('hello kitty.JPG');
+    caneca = loadImage('sky.JPG');
+    escultura = loadImage("panda.JPG");
+    lego = loadImage("panda.JPG");
+    popit = loadImage("picole.JPG");
+}
+
+function setup()
+{
+    canvas = createCanvas(400, 500);
+    canvas.center();
+
+    detec = ml5.objectDetector('cocossd', modelLoaded);
+
+    document.getElementById("status").innerHTML = "Detectando Objetos";
+}
+
+function modelLoaded()
+{
+    modelLoaded = true;
+    detec.detect(adesivo, gotResults);
+    detec.detect(lego, gotResults);
+    detec.detect(escultura, gotResults);
+    detec.detect(caneca, gotResults);
+    detec.detect(popit, gotResults);
+}
+
+function adesivo()
+{
+    window.location = "adesivo.html";
+}
+function caneca()
+{
+    window.location = "caneca.html";
+}
+function lego()
+{
+    window.location = "lego.html";
+}
+function escultura()
+{
+    window.location = "escultura.html";
+}
+function popit()
+{
+    window.location = "popit.html";
+}
+
+function voltar()
+{
+    window.location = "index.html";
+}
+function voltar2()
+{
+    window.location = "index.html";
+}
+function voltar3()
+{
+    window.location = "index.html";
+}
+function voltar3()
+{
+    window.location = "index.html";
+}
+function voltar4()
+{
+    window.location = "index.html";
+}
+function voltar5()
+{
+    window.location = "index.html";
+}
+
+function gotResults(error, results)
+{
+    {
+        if (error)
+        {
+            console.log(error);
+        }
+        console.log(results);
+        objects=results;
+    }
+}
